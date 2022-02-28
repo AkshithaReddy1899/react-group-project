9ac6cceff06518bb573f31523ac8025a674a3ef9
brytebee <brytebee@gmail.com>
2022-02-28 13:06:52 +0100

    :white_check_mark: Add logo
    Added logo to the header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import logo from '../assets/img/planet.png';
 
 const Header = () => (
   <nav>
+    <img
+      src={logo}
+      alt="Planets logo"
+      style={{ height: '80px', width: '80px' }}
+    />
     <ul>
       <li>
         <NavLink to="/">Rockets</NavLink>
