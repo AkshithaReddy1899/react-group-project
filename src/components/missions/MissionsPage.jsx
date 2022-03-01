aa25e2bc227c2ee22911ec4d0c7a0c0999e23d38
brytebee <brytebee@gmail.com>
2022-03-01 08:41:02 +0100

    :white_check_mark: Fetch data
    Fetched data only

--- src/components/missions/MissionsPage.jsx
+++ src/components/missions/MissionsPage.jsx
@@ -1,10 +1,11 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import Table from 'react-bootstrap/Table';
+// import Table from 'react-bootstrap/Table';
 import { getMissionsFromAPI } from '../../redux/missions/missions';
 
 const MissionsPage = () => {
   const missions = useSelector((state) => state.missionsReducer);
+  console.log(missions);
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -14,30 +15,6 @@ const MissionsPage = () => {
   return (
     <div>
       <h1>missions statement</h1>
-      <Table>
-        <thead>
-          <tr>
-            <th>#</th>
-            <th>Missions</th>
-            <th>Description</th>
-            <th>Status</th>
-            <th>{''}</th>
-          </tr>
-        </thead>
-        <tbody>
-          {missions.map((mission) => (
-            <tr key={mission.mission_id}>
-              <td>1</td>
-              <td>{mission.mission_name}</td>
-              <td>{mission.description}</td>
-              <td>Not a Member</td>
-              <td>
-                <button type="button">Join Mission</button>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </Table>
     </div>
   );
 };
