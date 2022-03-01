d30b8b147570d8cb67acf967a3015a05c4dbd32d
brytebee <brytebee@gmail.com>
2022-03-01 08:03:59 +0100

    :white_check_mark: Fetch Missions from API and Render
    Created action creators and fetch method to get data from API and render to the missions page.

---
+++ src/redux/missions/missions.js
@@ -0,0 +1,25 @@
+const GET_MISSIONS = 'MISSIONS/GET_MISSIONS';
+
+const getMissions = (payload) => ({
+  type: GET_MISSIONS,
+  payload,
+});
+
+export const getMissionsFromAPI = () => async (dispatch) => {
+  dispatch(
+    getMissions(
+      await (await fetch('https://api.spacexdata.com/v3/missions')).json(),
+    ),
+  );
+};
+
+const missionsReducer = (state = [], action) => {
+  switch (action.type) {
+    case GET_MISSIONS:
+      return action.payload;
+    default:
+      return state;
+  }
+};
+
+export default missionsReducer;
