d8e3c71bf4f4aa694b225b6d65918fea1c4187da
AkshithaReddy1899 <akshithareddy1899@gmail.com>
2022-03-02 16:07:59 +0530

    Remove console log

--- src/components/rockets/RocketsProfile.js
+++ src/components/rockets/RocketsProfile.js
@@ -3,8 +3,7 @@ import { useSelector } from 'react-redux';
 
 const RocketsProfile = () => {
   const Rockets = useSelector((state) => state.rockets.filter((rocket) => rocket.reserved));
-
-  console.log(Rockets.length);
+  
   return (
     <div className="rocket-profile">
       <h2>My Rockets</h2>
